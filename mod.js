e04e9d97d647bc04eb6dbeb641288957f7c0ddba
Nielene Thomas <nielenethomas@pursuit.org>
2018-12-09 16:47:21 -0500

    middleware part1 done

--- mod.js
+++ mod.js
@@ -1,104 +1,36 @@
 const express = require('express');
 const app = express();
-
+//------------------------------------
 let animalList = ["zebra", "tiger", "lion", "goat"]
-
-
-
+//------------------------------------
 const animalSearch = (req, res, next) => {
-  console.log(req.params.species);
-  let search = req.params.species;
-  let answer;
-  // let status;
-  //  // = res.status;
-  // let message;
-   // = res.message;
-  animalList.forEach(string => {
-    if (string === search) {
-      answer = {
-        status: "success",
-        message: true
-      }
-    } else {
-      answer = {
-        status: "failure",
-        message: false
-      }
+  res.status = "failure";
+  res.message = false;
+
+  animalList.forEach(species => {
+    if (req.params.species === species) {
+      res.status = "success";
+      res.message = true;
     }
   })
-  res.answer;
-  console.log(search, res.answer);
-  // res.status = status;
-  // res.message = message;
-  // console.log("here", res.status, res.message);
-
-  // console.log(res.status, res.message);
-  // res.json({thestatus:status, themessage:message})
   next()
 }
-
-
-
-
+//------------------------------------
 const finalFunction = (req, res) => {
-  console.log("hhhh", answer);
-  res.json({answer})
-  // res.json({status: theStatus, message: theMessage})
-  // console.log("here",req.params.species);
-  // res.json(res.msg)
-
-  // res.json({status, message})
-  // res.json({message: res.params.species})
-
-  // res.json({
-  //   status: ,
-  //   message:
-  // })
+  // console.log(res.status, res.message);
+  res.json({status: res.status, message: res.message})
 }
-
-
-
-
+//------------------------------------
 const error = (req, res) => {
   res.json({message: "👎👿404"});
 }
-
-app.get('/animal/:species', animalSearch, finalFunction)
-
+//------------------------------------
+app.get('/animal/:species',animalSearch,finalFunction)
 app.get('/*', error)
-
-
-
-
-
-
-
-
+//------------------------------------
 const port = 3500;
 app.listen(port, ()=> {
-  console.log('porting: ', port);
+  console.log('Your port IS: ', port);
 })
 
-//===============================
-
-// const middleware = (res, req, next) => {
-//   // console.log(req.params);
-//   const species = ['zebra', 'tiger', 'elephant']
-//
-//   res.json(
-//     let answer;
-//     for (let i = 0; i < species.length; i++) {
-//
-//       if (Object.value(req.params) === species[i]) {
-//         answer = true;
-//       } else {
-//         answer = false;
-//       }
-//     }
-//     return answer;
-//   )
-//
-//   next();
-// }
-//
-// app.get('/animal/:wildcard', middleware)
+//====================================
--- scrap.js
+++ scrap.js
@@ -1,8 +1,9 @@
+//===========declare default to failure adn success IF condition met. (corey said so) (working!)==========
+
 const express = require('express');
 const app = express();
 
 let animalList = ["zebra", "tiger", "lion", "goat"]
-
 
 const animalSearch = (req, res, next) => {
   res.status = "failure";
@@ -13,29 +14,20 @@ const animalSearch = (req, res, next) => {
       res.message = true;
     }
   })
-  // res.status = status;
-  // res.message = message;
-// req.params.species;
-// res.status = true;
-
   next()
 }
-
 
 const finalFunction = (req, res) => {
   console.log(res.status, res.message);
   res.json({status: res.status, message: res.message})
 }
 
-
 const error = (req, res) => {
   res.json({message: "👎👿404"});
 }
 
-
 app.get('/animal/:species',animalSearch,finalFunction)
 app.get('/*', error)
-
 
 const port = 3100;
 app.listen(port, ()=> {
@@ -43,7 +35,7 @@ app.listen(port, ()=> {
 })
 
 
-//=============================
+//===========using variables: (working) ===========
 
 
 // const express = require('express');
@@ -89,3 +81,5 @@ app.listen(port, ()=> {
 // app.listen(port, ()=> {
 //   console.log('porting: ', port);
 // })
+
+// ====================================
